894f10d21f4b9c5b5e640d4f88d812ccd9447b47
qq306736166 <306736166@qq.com>
2021-08-29 15:54:15 +0800

    修改和添加模板

--- src/com/controller/VueDictionary.java
+++ src/com/controller/VueDictionary.java
@@ -5,10 +5,17 @@ import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import com.sun.tools.javac.util.StringUtils;
 import sun.awt.Symbol;
 
+import java.lang.reflect.Method;
+
 public class VueDictionary extends StringUtils {
 
-    public VueDictionary() {
+    public VueDictionary(Method method) {
+        StringUtils stringUtils = new StringUtils();
+        if(super.equals(stringUtils)){
+            stringUtils.getClass().getClassLoader();
+        }else if(method.isAccessible()){
 
+        }
     }
 
     @VueDictionarys(type = "type", value = "String")
@@ -16,38 +23,52 @@ public class VueDictionary extends StringUtils {
         return "";
     }
 
-    @VueDictionarys(type = "type", value = "Number")
+    @VueDictionarys(type = "type", value = "@Number")
     private Integer number() {
         return 0;
     }
 
-    @VueDictionarys(type = "type", value = "Function")
+    @VueDictionarys(type = "type", value = "@Function")
     private Object function() {
         return new Object();
     }
 
-    @VueDictionarys(type = "type", value = "Undefined")
+    @VueDictionarys(type = "type", value = "@Undefined")
     private String undefined() {
         return "undefined";
     }
 
-    @VueDictionarys(type = "type", value = "Symbol")
+    @VueDictionarys(type = "type", value = "@Symbol")
     private Symbol symbol() {
         return new Symbol();
     }
 
-    @VueDictionarys(type = "type", value = "map")
+    @VueDictionarys(type = "type", value = "@map")
     private Object map() {
         return new Object();
     }
 
-    @VueDictionarys(type = "type", value = "error")
+    @VueDictionarys(type = "type", value = "@error")
     private Error error() {
         return new Error();
     }
 
-    @VueDictionarys(type = "hexToBinary", value = "hexToBinaryDic")
+    @VueDictionarys(type = "toType", value = "@hexToBinaryDic")
     private HexBin hexBin() {
         return new HexBin();
     }
+
+    @VueDictionarys(type = "doType", value = "@isString")
+    private Boolean isString() {return false;}
+
+    @VueDictionarys(type = "doType",value = "@isNumber")
+    private Boolean isNumber(){return false;}
+
+    @VueDictionarys(type = "doType",value = "@isFunction")
+    private Boolean isFunction(){return false;}
+
+    @VueDictionarys(type = "doType",value = "isObject")
+    private Boolean isObject(){return false;}
+
+
 }
--- src/com/controller/VueTemplate.java
+++ src/com/controller/VueTemplate.java
@@ -1,4 +1,18 @@
 package com.controller;
 
-public class VueTemplate {
+public class VueTemplate extends VueDictionary{
+
+    public VueTemplate(Object object) throws NoSuchMethodException {
+        super(object.getClass().getMethod("com.controller.*",VueDictionary.class));
+    }
+
+    private VueDictionary vueDictionary = new VueDictionary(this.getClass().getMethod("com.controller.*",VueTemplate.class));
+
+    public void setVueDictionary(VueDictionary vueDictionary) {
+        this.vueDictionary = vueDictionary;
+    }
+
+    public VueDictionary getVueDictionary() {
+        return vueDictionary;
+    }
 }
